f81814e655e40d3e6ed3480e6ed4a6e7c07c9585
proggeler <proggeler@de-mes.nl>
2025-02-14 23:06:28 +0100

    order of things

--- public/index.php
+++ public/index.php
@@ -11,6 +11,16 @@ $app = new App(
     container: (new ContainerFactory())->createContainer()
 );
 
+/* ************************************* *
+ *       Initialize the application      *
+ * ************************************* */
 $app->init();
+
+/* ************************************* *
+ *          Load configuration           *
+ * ************************************* */
+$app->loadConfig(__DIR__ . '/../config/');
+
+$app->initServices();
 
 $app->run();
--- src/Apps/App.php
+++ src/Apps/App.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace DMT\Apps;
 
+use DMT\Config\Config;
 use DMT\DependencyInjection\Container;
 use DMT\ServiceProviders\AppServiceProvider;
 use DMT\ServiceProviders\RoutingServiceProvider;
@@ -17,12 +18,37 @@ use Slim\App as BaseApp;
 class App extends BaseApp
 {
     /**
-     * Register all (installed) service providers.
+     * Register app service provider.
      */
     public function init(): void
     {
         $container = $this->getContainer();
+
         $container->register(provider: new AppServiceProvider($this));
+    }
+
+    /**
+     * Load the configuration file(s)
+     */
+    public function loadConfig(string $configPath): void
+    {
+        $files = [$configPath];
+        if (is_dir($configPath)) {
+            $files = glob($configPath . '/*.*');
+        }
+
+        foreach ($files as $file) {
+            $this->getContainer()->get(id: Config::class)->load($file);
+        }
+    }
+
+    /**
+     * Register installed service providers
+     */
+    public function initServices(): void
+    {
+        $container = $this->getContainer();
+
         $container->register(provider: new RoutingServiceProvider($this));
     }
 }
